0cd321268a5a00d2d5c59cf4ce96c060a2efd4cf
Kostyan847 <147474435+Kostyan847@users.noreply.github.com>
2023-12-24 22:16:10 +0500

    Update скрин15.py

--- задачи2/скрин15.py
+++ задачи2/скрин15.py
@@ -1,19 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov 18 10:42:31 2023
 
-@author: Pupil
-"""
-a='1, 2, 4, 5'
-f=1
-b=[]
-def Vika(a,f):
+a='98, 2, 40, 86'
+d=1
+l=[]
+def cjl(a,d):
    for i in range(len(a)):
        if a[i]!=" " and a[i]!=",":
            n=int(a[i])
-           b.append(n)      
-   print(b)   
-   for i in b:
-       f=f*i
-   print(f)
-Vika(a,f)
+           l.append(n)      
+   print(l)   
+   for i in l:
+       d=d*i
+   print(d)
+cjl(a,d)
