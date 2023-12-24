d161e07b3f02953e8de81af90e52877c3fbe08df
Kostyan847 <147474435+Kostyan847@users.noreply.github.com>
2023-12-25 04:36:14 +0500

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,6 +1,6 @@
 def mul_to_int(a, b):
-    x = a * b
-    if float(x).is_integer():
-        return int(x)
-    return x
+    p = a * b
+    if float(p).is_integer():
+        return int(p)
+    return p
 print(mul_to_int(1.5,2))
