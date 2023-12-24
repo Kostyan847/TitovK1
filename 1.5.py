8ca7f3179584b122c999d9b2732da616173dc16d
Kostyan847 <147474435+Kostyan847@users.noreply.github.com>
2023-12-25 04:21:15 +0500

    Delete 1.5.py

--- 1.5.py
+++
@@ -1,8 +0,0 @@
-def shourtner(st):
-    while '('in st or')' in st:
-        left = st.rfind('(')
-        right = st.rfind(')', left)
-        st = st.replace(st[left:right + 1], '')
-        return st
-print(shourtner('Биырвпып(вапы) бабах'))
-
